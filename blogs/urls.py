15570091546c604bd1c459548212620d684d9f20
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-17 11:51:12 -0500

    Update urls.py

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,5 +5,8 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('', views.index, name='index'),  #main page
-    path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail'), #page of specific blog
+    path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail'),
+    path('new_post/<int:blog_id>/', views.new_post, name = 'new_post'),
+    path('edit_post/<int:blog_id>', views.edit_post, name = 'edit_post'),
+    path('delete_post/<int:blog_id>/', views.delete_post, name = 'delete.post'),
 ]
